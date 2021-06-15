ce9af55ffbed6666275e736f0051fbba097dd0da
Hristian Bozinoski <konten.bozinoski@gmail.com>
2021-06-15 19:07:46 +0200

    JSON lint

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,9 @@ s = File.read("db/ingredients.json")
 
 obj = JSON.parse(s)
 
-obj.each do |ingredient|
-  ingredient.each do |stuff|
-    puts stuff
+obj.each do |external|
+  external[1].each do |internal|
+    p internal["name"]
   end
 end
 
