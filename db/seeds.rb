8a74579921f0e37acc936a6969fe92874f1f5249
Hristian Bozinoski <konten.bozinoski@gmail.com>
2021-06-15 18:56:15 +0200

    JSON lint

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-ingredients = {
+require 'json'
 
-}
+s = File.read("db/ingredients.json")
 
+obj = JSON.parse(s)
 
+obj.each do |ingredient|
+  ingredient.each do |stuff|
+    puts stuff
+  end
+end
 
-Ingredient.create(name: "Hampf Samen", price: 20, weight: 12, protein: 20)
+# Ingredient.create(name: "Hampf Samen", price: 20, weight: 12, protein: 20)
