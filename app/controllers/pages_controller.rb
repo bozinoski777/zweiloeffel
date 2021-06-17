88b529aaf49709b5acded00461e27dda00eb4b52
Hristian Bozinoski <konten.bozinoski@gmail.com>
2021-06-17 16:31:50 +0200

    Price summary on homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,12 +9,22 @@ class PagesController < ApplicationController
   def home
     @ingredients = Ingredient.all
     @doses = Dose.all
+    summary
   end
 
   def new_dose
     @dose = Dose.new(ingredient_id: params[:ingredient], amount: params[:amount], user_id: 1)
     @dose.save!
     redirect_to root_path
+  end
+
+  def summary
+    doses = Dose.all
+    summary_price = 0
+    doses.each do |dose|
+      summary_price += dose.ingredient.price
+    end
+    @summary_price = summary_price
   end
 
   def delete_all_doses
