a94c96f8299d320cd34c1a1da6737f34072ec1ce
Hristian Bozinoski <konten.bozinoski@gmail.com>
2021-06-21 20:49:41 +0200

    actioncable uninstall

--- app/channels/dose_channel.rb
+++
@@ -1,9 +0,0 @@
-class DoseChannel < ApplicationCable::Channel
-  def subscribed
-     stream_for Dose.all
-  end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-end
