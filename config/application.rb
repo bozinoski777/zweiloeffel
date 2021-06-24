e843b528989091acba590ea2c698cf3502340f9f
Hristian Bozinoski <konten.bozinoski@gmail.com>
2021-06-24 20:53:35 +0200

    deleteing stimulus

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module Zweiloeffel
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.action_view.embed_authenticity_token_in_remote_forms = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
