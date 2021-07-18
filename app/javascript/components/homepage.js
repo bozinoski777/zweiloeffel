276d3a7f8b62a8102fb09394e103029283870cc2
Hristian Bozinoski <konten.bozinoski@gmail.com>
2021-07-18 12:09:29 +0200

    JS Price DOM drilldown dome

--- app/javascript/components/homepage.js
+++ app/javascript/components/homepage.js
@@ -3,9 +3,24 @@ const initialData = () => {
   let weight = 0;
   let protein = 0;
 
-  document.getElementById(event.path[2].id).addEventListener("click", increaseOrder);
+
+  document.querySelectorAll('.selectionCard').forEach(item => {
+    item.addEventListener('click', event => {
+      // console.log(event.path[2].id);
+      console.log(event.path[2].children[0].children[1].children[1].innerHTML);
+
+
+
+
+    })
+  })
+
+
+  // let selectionCards = document.getElementsByClassName("selectionCard");
+
+  // selectionCards.addEventListener("click", increaseOrder);
   function increaseOrder() {
-    console.log(event);
+    // console.log(event);
 
     // let currentSelection = event.path[2].id;
     // console.log(event.path[2].id);
@@ -24,3 +39,14 @@ const initialData = () => {
 }
 
 export { initialData };
+      // let currentSelection = event.path[2].id;
+      // console.log(event.path[2].id);
+      // // Increase price
+      // price += selectionCard-pricing
+      // document.getElementById("price").innerHTML = price;
+      // // Increase weight
+      // weight += <%= ingredient_within.weight %>
+      // document.getElementById("weight").innerHTML = weight;
+      // // Increase protein
+      // protein += <%= ingredient_within.protein %>
+      // document.getElementById("protein").innerHTML = protein;
