ebb0cbfcdaf092621c5f41ecf21348b37fc6caff
Hristian Bozinoski <konten.bozinoski@gmail.com>
2021-07-19 15:43:10 +0200

    IMG selection in jar correct, no doubles

--- app/javascript/components/homepage.js
+++ app/javascript/components/homepage.js
@@ -2,6 +2,7 @@ let initialData = () => {
   let price = 0;
   let weight = 0;
   let protein = 0;
+  let selection = [];
   document.querySelectorAll('.selectionCard').forEach(item => {
     item.addEventListener('click', event => {
       // Fetch ingredient data from DOM
@@ -21,22 +22,41 @@ let initialData = () => {
       protein += ingredientProtein
       document.getElementById("finalProtein").innerHTML = protein;
 
+      // create array of selected elements
+      if(!selection.includes(item)) {
+        selection.push(item);
+      }
+      let counter = 0
+
+      selection.forEach(item => {
+        let selectedIngredientIMG = event.path[1].dataset.img;
+        let selectedIngredientWeight = parseInt(event.path[1].dataset.weight);
+
+
+        let contentImg = document.getElementById(`jar-img-${counter}`);
+        contentImg.src = selectedIngredientIMG
+        contentImg.style.height = `${selectedIngredientWeight}%`
+        counter += 1
+
+      })
+
       //Jar  This is where I left off. I dont know how to iterate over the coices to differentiate between alreadz selected or novel
-      let allContent = [...Array(8).keys()];
-      allContent.forEach(number => {
-        if (document.getElementById(`jar-img-${number}`).src === "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg==") {
-          console.log(document.getElementById(`jar-img-${number}`).src)
-        } else {
-          console.log("empty?");
+
+
+      // let contentImg = document.getElementById("jar-img-1");
+      // contentImg.src = ingredientIMG
+      // contentImg.style.height = `${weight}%`
+      let allDivs = [...Array(8).keys()];
+      allDivs.forEach(number => {
+        if (selection[number]) {
+          document.getElementById(`jar-img-${number}`).src = (selection[number]).dataset.img;
+          // console.log(selection[number].dataset.img)
         }
+
       });
-
-
-      let contentImg = document.getElementById("jar-img-1");
-      contentImg.src = ingredientIMG
-      contentImg.style.height = `${weight}%`
 
     })
   })
+
 }
 export { initialData };
