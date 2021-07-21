d939eac98138a8db9fbf5f9dec76d038778e1868
Hristian Bozinoski <konten.bozinoski@gmail.com>
2021-07-21 20:41:11 +0200

    IMG selection in jar displays correct

--- app/javascript/components/homepage.js
+++ app/javascript/components/homepage.js
@@ -26,33 +26,36 @@ let initialData = () => {
       if(!selection.includes(item)) {
         selection.push(item);
       }
-      // let counter = 0
+      let counter = 0
 
-      // selection.forEach(item => {
-      //   let selectedIngredientIMG = event.path[1].dataset.img;
-      //   let selectedIngredientWeight = parseInt(event.path[1].dataset.weight);
-      //   let contentImg = document.getElementById(`jar-img-${counter}`);
-      //   contentImg.src = selectedIngredientIMG
-      //   contentImg.style.height = "20%"
-      //   counter += 1
-      // })
+      selection.forEach(item => {
+        let selectedIngredientIMG = event.path[1].dataset.img;
+        let selectedIngredientWeight = parseInt(event.path[1].dataset.weight);
+        let contentImg = document.getElementById(`jar-img-${counter}`);
+
+        counter += 1
+          let currentHeight = parseInt(contentImg.style.height.replace("px", ""));
+          currentHeight += 50;
+          currentHeight += "px";
+          contentImg.style.height = currentHeight;
+      })
 
  // OR
     //  display none all divs that are empty, when the increase of height happens should be only top the selected item not all
+          console.log(ingredinetName)
+          console.log()
       let allDivs = [...Array(8).keys()];
       allDivs.forEach(number => {
         if (selection[number]) {
          let selectedImg = document.getElementById(`jar-img-${number}`);
           selectedImg.src = (selection[number]).dataset.img;
-          let currentHeight = parseInt(selectedImg.style.height.replace("px", ""));
-          console.log(currentHeight)
-          currentHeight += 50;
-          currentHeight += "px";
-          selectedImg.style.height = currentHeight;
-        } else {
+          selectedImg.dataset.name = ingredinetName
+
+
         }
 
       });
+
 
     })
   })
